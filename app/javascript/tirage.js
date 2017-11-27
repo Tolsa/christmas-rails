589de59147bb7a7b6ec8d25cc6b78d1aad6cb84b
Francois <rabanel.francois@gmail.com>
2017-11-27 21:23:57 +0100

    modification js par manou, correction des doublons

--- app/javascript/tirage.js
+++ app/javascript/tirage.js
@@ -5,29 +5,40 @@ function tirage(){
     event.preventDefault();
     const names = document.getElementById('inputnames').value;
     const liste = document.getElementById('liste');
-    liste.insertAdjacentHTML('beforeend', `<li>${names}</li>`)
+    liste.insertAdjacentHTML('afterbegin', `<li>${names}</li>`)
     array.push(names);
     document.getElementById('inputnames').value = "";
   });
+
   const button = document.getElementById('melange');
   button.addEventListener("click", (event) =>{
-    event.preventDefault();
-    document.getElementById("create_ul_cadeau").innerHTML = '<ul id="cadeau"></ul>';
-    const arr1 = array.slice(),
-          arr2 = array.slice();
-    arr1.sort(function() { return 0.5 - Math.random();});
-    arr2.sort(function() { return 0.5 - Math.random();});
-    const cadeau = document.getElementById('cadeau');
-    while (arr1.length) {
-        const name1 = arr1.pop(),
-              name2 = arr2[0] === name1 ? arr2.pop() : arr2.shift();
-              if (name1 === name2){
-                document.getElementById('cadeau').innerHTML += `<li class="delete">Oups, veuillez remélanger pour cette fois. Il y a eu un doublon.</li>`;
-              };
-        document.getElementById('cadeau').innerHTML += `<li class="delete">${name1} offre un cadeau à ${name2}</li>`;
+    if (array.length > 3){
+
+
+      event.preventDefault();
+      document.getElementById("create_ul_cadeau").innerHTML = '<ul id="cadeau"></ul>';
+      const arr1 = array.slice(),
+            arr2 = array.slice();
+
+      arr1.sort(function() { return 0.5 - Math.random();});
+      arr2.sort(function() { return 0.5 - Math.random();});
+
+      const cadeau = document.getElementById('cadeau');
+      while (arr1.length) {
+          const name1 = arr1.pop(),
+                name2 = arr2[0] === name1 ? arr2.pop() : arr2.shift();
+                if (name1 && name2 && name1 == name2){
+                  document.getElementById('cadeau').innerHTML += `<li class="delete">Oups, veuillez remélanger pour cette fois. Il y a eu un doublon.</li>`;
+                };
+          document.getElementById('cadeau').innerHTML += `<li class="delete">${name1} offre un cadeau à ${name2}</li>`;
+      }
+    } else {
+      alert('pas assez de noms');
     }
   });
+
 }
 
 
 export { tirage };
+
